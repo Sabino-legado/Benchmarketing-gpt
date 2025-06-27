cc970492fce444447f53ed2fa4d3ba0d5851464a
Sabino-legado <sabinokalufele@gmail.com>
2025-06-27 21:35:26 +0100

    Update search.js

--- api/search.js
+++ api/search.js
@@ -1,9 +1,10 @@
 import Typesense from 'typesense';
 
+console.log('TYPESENSE_CLUSTER:', process.env.TYPESENSE_CLUSTER); // Adicione isso
 const client = new Typesense.Client({
   nodes: [
     {
-      host: process.env.TYPESENSE_CLUSTER, // Correção: Usa TYPESENSE_CLUSTER
+      host: process.env.TYPESENSE_CLUSTER,
       port: 443,
       protocol: 'https',
     },
@@ -33,7 +34,7 @@ export default async function handler(req, res) {
       dados: resultados.hits.map((hit) => hit.document),
     });
   } catch (erro) {
-    console.error('Erro no Typesense:', erro); // Log para debug
+    console.error('Erro no Typesense:', erro);
     return res.status(500).json({
       erro: 'Erro ao buscar dados no Typesense',
       detalhes: erro.message,
